bb25280ad16c2848d4d9b0ac4d43a642c6e14b86
LuongXuanNhat <xuannhat1932002@gmail.com>
2024-03-24 17:30:23 +0700

    fix reset pass

--- StartupNNTM/Service/AccountService.cs
+++ StartupNNTM/Service/AccountService.cs
@@ -133,15 +133,13 @@ namespace StartupNNTM.Service
             }
 
             var confirmNumber = GetConfirmCode();
-            await SaveCodeConfirm(confirmNumber, email);
 
-
-            await SendConfirmCodeToEmail(user.Email, confirmNumber);
+            await SendPasswordToEmail(user.Email, confirmNumber);
             var obj = await ConfirmCode(email);
 
             // reset
-            obj.Password = confirmNumber;
-            return await ResetPassword(obj);
+            obj.Password = "NoViSo@"+confirmNumber;
+            return await ResetPassword(obj, user);
         }
         public async Task<ApiResult<string>> Register(RegisterRequest request)
         {
@@ -247,7 +245,7 @@ namespace StartupNNTM.Service
                         </h1>
                       </header>
                       <div class=""panel__content"" style=""font-family: Montserrat; font-size: 14px;"">
-                        Xin chào! Chúng tôi đã nhận yêu cầu xác thực tài khoản web <strong>Tên website</strong> của bạn. Mã dùng một lần của bạn là:
+                        Xin chào! Chúng tôi đã nhận yêu cầu xác thực tài khoản web <strong>NoViSo</strong> của bạn. Mã dùng một lần của bạn là:
                         <br>
                         <div style=""font-size: 4em; text-align: center"">
                           {confirmNumber}
@@ -270,7 +268,96 @@ namespace StartupNNTM.Service
 
             await _sendmailservice.SendMail(content);
         }
+        private async Task SendPasswordToEmail(string email, string confirmNumber)
+        {
+            MailContent content = new()
+            {
+                To = email,
+                Subject = "Mật khẩu mới cho tài khoản NoViSo [Nông nghiệp thông minh & thực phẩm sạch]",
+                Body = $@"<!DOCTYPE html>
+                <html lang=""en"">
+                <head>
+                  <meta charset=""UTF-8"">
+                  <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+                  <style>
+                    :root {{
+                      --panel-color: rgba(0, 0, 0, 0.135);
+                      --title-color: rgba(37, 47, 61, 1);
+                      --panel-border-width: 0.1em;
+                      --panel-padding: 0.75em;
+                    }}
 
+                    .panel {{
+                      background: var(--panel-color);
+                      border-radius: var(--panel-border-width);
+                      padding: var(--panel-border-width);
+                    }}
+                    .panel__header {{
+                        background: var(--title-color);
+                    }}
+                    .panel__header, 
+                    .panel__content {{
+                      padding: var(--panel-padding);
+                    }}
+
+                    .panel__title {{
+                      line-height: 1;
+                      font-family: Montserrat;
+                    }}
+
+                    .panel__content {{
+                      padding: 12px 22px;
+                      background: #fff;
+                    }}
+
+                    .example {{
+                      display: flex;
+                      flex-grow: 1;
+                      padding: 1em max(1em, calc(50vw - 60ch));
+                      place-items: center;
+                    }}
+
+                    .example > * {{
+                      flex-grow: 1;
+                    }}
+
+                    .bg-1 {{
+                      background-image: linear-gradient( 135deg, #81FBB8 10%, #28C76F 100%);
+                    }}
+                  </style>
+                </head>
+                <body>
+                  <div class=""example bg-1"">
+                    <section class=""panel"">
+                      <header class=""panel__header"">
+                        <h1 class=""panel__title"" style=""text-align: center"">
+                          Nông nghiệp thông minh & thực phẩm sạch
+                        </h1>
+                      </header>
+                      <div class=""panel__content"" style=""font-family: Montserrat; font-size: 14px;"">
+                        Xin chào! Chúng tôi đã nhận yêu cầu quên mật khẩu từ tài khoản web <strong>NoViSo</strong> của bạn. Mật khẩu mới của bạn là:
+                        <br>
+                        <div style=""font-size: 4em; text-align: center"">
+                          {confirmNumber}
+                        </div>
+                        <hr>
+                        <div style=""padding-top: 12px; text-align: center; font-family: Montserrat"">
+                        Chúng tôi sẽ không bao giờ gửi email cho bạn và yêu cầu bạn tiết lộ hoặc xác minh mật khẩu, thẻ tín dụng hoặc số tài khoản ngân hàng của bạn.
+                        </div>
+                        </div>
+
+                        <div style=""padding: 12px 0px; text-align: center; font-family: Montserrat; font-size: 10px"">
+                        Thông báo này được tạo và phân phối bởi ***, Inc.., *** là thương hiệu đã đăng ký của ***, Inc. Xem chính sách quyền riêng tư của chúng tôi.
+                        </div>
+                    </section>
+                  </div>
+                </body>
+                </html>"
+            };
+
+
+            await _sendmailservice.SendMail(content);
+        }
         private string GetConfirmCode()
         {
             return new Random().Next(100000, 999999).ToString();
@@ -292,22 +379,13 @@ namespace StartupNNTM.Service
             return principal;
         }
 
-        public async Task<ApiResult<string>> ResetPassword(ResetPassDto resetPass)
+        public async Task<ApiResult<string>> ResetPassword(ResetPassDto resetPass,User user)
         {
-            var user = await _userManager.FindByEmailAsync(resetPass.Email);
-            if (user is null) return new ApiErrorResult<string>("Lỗi");
             var resetPasswordResult = await _userManager.ResetPasswordAsync(user, resetPass.Token, resetPass.Password);
             if (!resetPasswordResult.Succeeded)
             {
               //  _logger.LogError("Xảy ra lỗi trong quá trình xử lý | ", resetPasswordResult.Errors.Select(e => e.Description));
                 return new ApiErrorResult<string>("Lỗi! "+ resetPasswordResult.Errors.Select(e => e.Description));
-            }
-            if (user.AccessFailedCount is 5)
-            {
-                user.LockoutEnabled = false;
-                user.AccessFailedCount = 0;
-                _dataContext.User.Update(user);
-                await _dataContext.SaveChangesAsync();
             }
             return new ApiSuccessResult<string>();
         }
